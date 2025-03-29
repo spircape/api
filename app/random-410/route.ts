f24a366ed2d79d23e160f6eb89865f907556880c
Kegongteng <i@kegongteng.cn>
2025-03-29 21:11:23 +0800

    Update route.ts

--- app/random-410/route.ts
+++ app/random-410/route.ts
@@ -2,7 +2,7 @@ import { type NextRequest, NextResponse } from "next/server"
 
 export async function GET(request: NextRequest) {
   // 生成1到50之间的随机ID
-  const randomId = Math.floor(Math.random() * 50) + 1
+  const randomId = Math.floor(Math.random() * 60) + 1
   // 构建随机图片URL
   const randomImageUrl = `https://zh.yuazhi.cn/at410/random/${randomId}.jpg`
 
