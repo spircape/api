45b09e41f5f86d5c3b4a53a79c003c3f8813e8e1
Kegongteng <i@kegongteng.cn>
2025-10-04 17:39:09 +0800

    chore：update 410 data

--- app/random-410/route.ts
+++ app/random-410/route.ts
@@ -4,7 +4,7 @@ export async function GET(request: NextRequest) {
  // 生成1到50之间的随机ID
  const randomId = Math.floor(Math.random() * 100) + 1
  // 构建随机图片URL
- const randomImageUrl = `https://zh.yuazhi.cn/at410/random/${randomId}.jpg`
+ const randomImageUrl = `https://box.zh.yuazhi.cn/410/note/${randomId}.jpg`
 
   // 重定向到随机图片
   return NextResponse.redirect(randomImageUrl)
